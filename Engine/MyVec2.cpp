0863850254cbf79b245d632af51bf42294d95fe0
Zedtho <Zedtho@users.noreply.github.com>
2018-02-19 20:24:34 +0100

    Added operators to MyVec2 class

--- Engine/MyVec2.cpp
+++ Engine/MyVec2.cpp
@@ -12,10 +12,34 @@ MyVec2 MyVec2::operator+(const MyVec2 & rhs) const
 	return MyVec2(x + rhs.x, y + rhs.y);
 }
 
+MyVec2 & MyVec2::operator+=(const MyVec2 & rhs)
+{
+	return *this = *this + rhs;
+}
+
 MyVec2 MyVec2::operator-(const MyVec2 & rhs) const
 {
 	return MyVec2(x - rhs.x, y - rhs.y);
 }
+
+MyVec2 & MyVec2::operator-=(const MyVec2 & rhs)
+{
+	return *this = *this - rhs;
+}
+
+MyVec2 MyVec2::operator*(float scalar) const
+{
+	return MyVec2(x * scalar, y * scalar );
+}
+
+MyVec2 & MyVec2::operator*=(const float rhs)
+{
+	return *this = *this * rhs;
+}
+
+
+
+
 
 float MyVec2::GetX() const
 {
--- Engine/MyVec2.h
+++ Engine/MyVec2.h
@@ -6,7 +6,11 @@ public:
 	MyVec2() = default;
 	MyVec2(float x_in, float y_in);
 	MyVec2 operator+(const MyVec2& rhs) const;
+	MyVec2& operator+=(const MyVec2& rhs);
 	MyVec2 operator-(const MyVec2& rhs) const;
+	MyVec2 & operator-=(const MyVec2& rhs);
+	MyVec2 operator*(float rhs) const;
+	MyVec2& operator*=(const float rhs);
 	float GetX() const;
 	float GetY() const;
 public:
